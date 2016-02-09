ba431062c115469b88567d2c68b7d4e84b1e255a
Willi Ballenthin <wilbal1087@gmail.com>
2016-02-08 22:09:20 -0500

    init import

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='ida-netnode',
+      version="0.1",
+      description="Humane API for storing and accessing persistent data in IDA Pro databases",
+      author='Willi Ballenthin',
+      author_email='william.ballenthin@fireeye.com',
+      url='https://github.com/williballenthin/ida-netnode',
+      license='Apache License (2.0)',
+      packages=['netnode'],
+      classifiers = ["Programming Language :: Python",
+                     "Programming Language :: Python :: 2",
+                     "Operating System :: OS Independent",
+                     "License :: OSI Approved :: Apache Software License"],
+     )
