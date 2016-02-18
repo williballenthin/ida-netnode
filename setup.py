d3ea75a5e013ec9ccd100431c84fb813fc56e523
Willi Ballenthin <wilbal1087@gmail.com>
2016-02-18 10:52:47 -0500

    setup.py: bump to v1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='ida-netnode',
-      version="0.1",
+      version="1.0",
       description="Humane API for storing and accessing persistent data in IDA Pro databases",
       author='Willi Ballenthin',
       author_email='william.ballenthin@fireeye.com',
